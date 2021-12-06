5e4c699f36e19c1bb951c9db857f1582b0cd6801
luoqc <luoqc@opstime.xin>
2021-12-06 20:40:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+## Celery后台管理系统
+* 采用前后端分离的模式，前端基于[Saber](https://gitee.com/smallc/Saber)开发
+* 后端采用SpringBoot，基于[SpringBlade boot 3.1.0版本](https://gitee.com/smallc/SpringBlade)开发
+
+## 如何启动
+```
+$ git clone https://gitee.com/llqcai/celery-admin.git
+$ cd celery-admin
+# 安装
+$ yarn install
+# 启动
+$ yarn run serve     
+```
---
+++ src/views/wel/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <el-col :span="24">
+      <basic-container>
+        <el-collapse v-model="activeNames" @change="handleChange">
+          <el-collapse-item title="拥有的核心功能" name="1">
+            <div>1.开发中...</div>
+          </el-collapse-item>
+          <el-collapse-item title="软件定制开发合作" name="2">
+            <div>1.接BladeX系列架构的定制服务</div>
+            <div>2.接3个月以内工期的react、vue、springboot、springcloud、app、小程序等软件定制服务</div>
+            <div>3.有意向请联系唯一指定微信:lqc0097</div>
+          </el-collapse-item>
+        </el-collapse>
+      </basic-container>
+    </el-col>
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from "vuex";
+
+  export default {
+    name: "wel",
+    data() {
+      return {
+        activeNames: ['1', '2', '3', '5'],
+        logActiveNames: ['26']
+      };
+    },
+    computed: {
+      ...mapGetters(["userInfo"]),
+    },
+    created() {
+    },
+    methods: {
+      handleChange(val) {
+        console.log(val);
+      }
+    }
+  };
+</script>
+
+<style>
+  .el-divider--horizontal {
+    margin: 12px 0 !important;
+  }
+
+  .el-font-size {
+    font-size: 14px;
+  }
+</style>
+
